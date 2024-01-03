74c64bcaf3f1b832e9bf96871072b58a1d902dd1
RainBowT <rainbowt0506@gmail.com>
2024-01-04 04:25:17 +0800

    feat: Add India states' geographic information and create mapping for state codes

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
-# This is a sample Python script.
+import plotly.io as pio
+import json
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+pio.renderers.default = 'browser'
 
+india_states = json.load(open("states_india.geojson", "r"))
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+state_id_map = {}
+for feature in india_states["features"]:
+    feature["id"] = feature["properties"]["state_code"]
+    state_id_map[feature["properties"]["st_nm"]] = feature["id"]
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print("India States ID Map:")
+print(state_id_map)
